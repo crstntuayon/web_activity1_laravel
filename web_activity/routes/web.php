f77815e5a129eb1f5a5f787e5b9f830e76fc0ae4
crese <crestuayon@gmail.com>
2025-02-25 12:45:04 -0700

    Add new user

--- web_activity/routes/web.php
+++ web_activity/routes/web.php
@@ -1,11 +1,32 @@
 <?php
 
-use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentController;
 
 
+Route::get('/', function () {
+    return view('home');
+});
 
-Route::get('/students', [StudentController::class, 'index']);
-Route::post('/students', [StudentController::class, 'store']);
-Route::get('/students/{id}/edit', [StudentController::class, 'edit']);
-Route::put('/students/{id}', [StudentController::class, 'update']);
+
+// Protected Routes (Authenticated Users Only)
+Route::middleware('auth')->group(function () {
+
+    // Students Routes
+    Route::get('/students', [StudentController::class, 'index'])->name('students.index');  // Protected
+    Route::get('/students/search', [StudentController::class, 'search'])->name('students.search');  // Protected
+
+    // Student Edit Routes
+    Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');  // Protected
+    Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');  // Protected
+
+    // Student Delete Routes
+    //Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');  // Protected
+
+    // Add new student (protected)
+    Route::post('/add-new', [StudentController::class, 'addNewStudent'])->name('std.addNewStudent');  // Protected
+});
+
+// Public Students View Route (If needed to be public)
+Route::get('/', [StudentController::class, 'myView'])->name('std.myView'); // Public if required
